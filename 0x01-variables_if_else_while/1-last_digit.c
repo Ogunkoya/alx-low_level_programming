eba91942c5f43c6d6de4f4d4faf583846702568a
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-01 16:36:44 -0700

    Task 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ else if (last == 0)
 }
 else if ((last < 6) && (last != 0))
 {
-	printf("Last digit of %d is %d and is less than 6 and not 0", n, last);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 }
 
 return (0);
