3abf1d5672fdb1d4e24a066410fd22650242de0b
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-01 21:36:20 -0700

    Task 12clear

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,109 +1,57 @@
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 
- */
-
 #include <stdio.h>
 
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-
 /**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
   
 {
-  
-  int p = 48, q = 48, y = 48, x = 49;
-  
-
-  
-  while (p < 58)
-    
-    {
-      
-      while (q < 58)
+int ch1;
+int ch2;
+int ch3;
+int ch4;
 	
+ch1 = 48;
+ch2 = 48;
+ch3 = 48;
+ch4 = 49;
+while (ch1 < 58)
+{
+	while (ch2 < 58)
 	{
-	  
-	  while (y < 58)
-	    
-	    {
-	      
-	      while (x < 58)
-		
+		while (ch3 < 58)
 		{
-		  
-		  putchar(p);
-		  
-		  putchar(q);
-		  
-		  putchar(' ');
-		  
-		  putchar(y);
-		  
-		  putchar(x);
-		  
-		  if (!(p == 57 &&
-			
-			q == 56 &&
-			
-			y == 57 &&
-			
-			x == 57))
-		    
-		    {
-		      
-		      putchar(',');
-		      
-		      putchar(' ');
-		      
-		    }
-		  
-		  x++;
-		  
+			while (ch4 < 58)
+			{
+				putchar(ch1);
+				putchar(ch2);
+				putchar(32);
+				putchar(ch3);
+				putchar(ch4);
+				if (!(ch1 == 57 && ch2 == 56 && ch3 == 57 &&
+							ch4 == 57))
+				{
+					putchar(44);
+					putchar(32);
+				}
+				ch4++;
+			}
+			ch3++;
+			ch4 = 48;
 		}
-	      
-	      y++;
-	      
-	      x = 48;
-	      
-	    }
-	  
-	  q++;
-	  
-	  q = p;
-	  
-	  x = q + 1;
-	  
+		ch2++;
+	       	ch2 = ch1;
+		ch4 = ch2 + 1;
 	}
-      
-      p++;
-      
-      q = 48;
-      
-      y = p;
-      
-      x = q+ 1;
-      
-    }
-  
-  putchar('\n');
-  
-  return (0);
-  
+	ch1++;
+	ch2 = 48;
+	ch3 = ch1;
+	ch4 = ch2 + 1;
 }
+putchar(10);
   
+return (0);  
+}
