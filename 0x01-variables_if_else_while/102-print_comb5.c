00f23493df0bc5f9e1675a8a9e461925678a91ad
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-01 21:53:22 -0700

    Task 12clear

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,57 +1,45 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
 * main - Entry point
 *
+* Description: 'the program's description'*
+* 
 * Return: Always 0 (Success)
+*
 */
 
 int main(void)
-  
 {
-int ch1;
-int ch2;
-int ch3;
-int ch4;
-	
-ch1 = 48;
-ch2 = 48;
-ch3 = 48;
-ch4 = 49;
-while (ch1 < 58)
-{
-	while (ch2 < 58)
+	int ch1;
+	int ch2;
+	int ch3;
+	int ch4;
+	for (ch1 = 48; ch1 <= 57; ch1++)
 	{
-		while (ch3 < 58)
-		{
-			while (ch4 < 58)
+		for (ch2 = 48; ch2 <= 56; ch2++)
+		{						
+			for (ch3 = 48; ch3 <= 57; ch3++)
 			{
-				putchar(ch1);
-				putchar(ch2);
-				putchar(32);
-				putchar(ch3);
-				putchar(ch4);
-				if (!(ch1 == 57 && ch2 == 56 && ch3 == 57 &&
-							ch4 == 57))
+				for (ch4 = 49; ch4 <= 57; ch4++)
 				{
-					putchar(44);
+					putchar(ch1);
+					putchar(ch2);
 					putchar(32);
+					putchar(ch3);
+					putchar(ch4);
+					if (ch1 != 57 && ch2 != 56 && ch3 != 57
+							&& ch4 != 57)
+					{
+						putchar(44);
+					}
 				}
-				ch4++;
 			}
-			ch3++;
-			ch4 = 48;
 		}
-		ch2++;
-	       	ch2 = ch1;
-		ch4 = ch2 + 1;
 	}
-	ch1++;
-	ch2 = 48;
-	ch3 = ch1;
-	ch4 = ch2 + 1;
-}
-putchar(10);
-  
-return (0);  
+	putchar(10);
+	
+return (0);
 }
