af6a599ef2c88cdc0c3b5a13559428d56bcac37e
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-01 18:33:57 -0700

    Task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int ch1;
+int ch2;
+
+for (ch1 = 48; ch1 <= 57; ch1++)
+{
+	for (ch2 = 0; ch2 <= 57; ch2++)
+	{
+		putchar(ch1);
+		putchar(ch2);
+		if (ch1 != 56 || ch2 != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+}
+putchar(10);
+
+return (0);
+}
