2ffce4d601dd27456ebfe1344716c2f0f65d4658
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-01 17:32:16 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int n;
+
+for (n = 1; n <= 9; n++)
+{
+	printf("%d", n)
+}
+putchar(10);
+
+return (0);
+}
