b2f7cd2d36b6987e68b0c03c313de5fc103c49e8
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-01 20:09:32 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,15 +14,28 @@ int main(void)
 {
 int ch1;
 int ch2;
+int ch3;
+int ch4;
 
 for (ch1 = 48; ch1 <= 57; ch1++)
 {
-	for (ch2 = 48; ch2 <= 57; ch2++)
+	for (ch2 = 48; ch2 <= 56; ch2++)
 	{
-		putchar(ch1);
-		putchar(ch2);
-		putchar(44);
-		putchar(32);
+		for (ch3 = 48; ch3 <= 57; ch3++)
+		{
+			for (ch4 = 49; ch4 <= 57)
+			{
+				putchar(ch1);
+				putchar(ch2);
+				putchar(44);
+				putchar(ch3);
+				putchar(ch4);
+				if (ch1 != 57 && ch2 != 56 && ch3 != 57 && ch4 != 57)
+				{
+					putchar(32);
+				}
+			}
+		}
 	}
 }
 putchar(10);
