4e5fd1b02bfa2d43abd79b2d03e1bf9ff3e813b1
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-01 19:21:44 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int ch1;
+int ch2;
+int ch3;
+
+for (ch1 = 48; ch1 <= 57; ch1++)
+{
+	for (ch2 = 49; ch2 <= 57; ch2++)
+		for (ch3 = 50; ch3 <= 57; ch++)
+		{
+			{
+				if (ch2 > ch1 && ch3 > ch2)
+				{
+					putchar(ch1);
+					putchar(ch2);
+					putchar(ch3);
+					if (ch1 != 56 || ch2 != 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+}
+putchar(10);
+
+return (0);
+}
