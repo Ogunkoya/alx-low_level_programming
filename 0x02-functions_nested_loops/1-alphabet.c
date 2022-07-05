9d6286d344879e79242799cfebfca460436b4811
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-05 15:17:22 -0700

    lowere case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+
+return (0);
+}
