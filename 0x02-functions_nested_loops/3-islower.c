9d510cd6834bff440d91f9e258198913183fde1f
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-05 16:39:53 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _islower - check if a letter is lower capital
+*
+* @c: a character argument
+*
+* Description: 'the function will print out lower case alphabet'
+*
+* Return: return no value
+*/
+
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
