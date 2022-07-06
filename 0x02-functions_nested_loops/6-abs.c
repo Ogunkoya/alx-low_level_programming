90304c560c19a4fa8cbc7f95187055b8c6b37ea8
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-05 17:19:18 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _abs - return absolute value
+*
+* @n: a character argument
+*
+* Description: 'the function will print out lower case alphabet'
+*
+* Return: return no value
+*/
+
+int _abs(int n)
+{
+if (n < 0)
+	n = -n;
+return (n);
+}
