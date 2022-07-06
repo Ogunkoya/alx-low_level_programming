712812b7b400a052ddfe3242d1932e5dca8b8d3d
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-05 17:35:31 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_last_digit - check if a letter is lower capital
+*
+* @n: a character argument
+*
+* Description: 'the function will print out lower case alphabet'
+*
+* Return: return no value
+*/
+
+int print_last_digit(int n)
+{
+int r;
+
+if (n < 0)
+	n = -n;
+r = n % 10;
+return (r + '0');
+}
