5cdead57209e2627ebb68a0e7bc3dd784555890f
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-06 14:51:05 -0700

    Task 7

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,16 +21,24 @@ for (i = 0; i < 24; i++);
 		if (i < 10)
 		{
 			_putchar('0');
-			_putchar(i + '0')
+			_putchar(i + '0');
 		}
 		else if (i >= 10)
 		{
-			_putchar((i / 10) + '0')
-			_putchar((i % 10) + ''0')
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + ''0');
+		}
+		if (j < 10)
+		{
+			_putchar(':');
+			_putchar('0');
+			_putchar(j + '0');
+		}
+		else if (j >= 10)
+		{
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 	}
-}
-r = n % 10;
-_putchar('0' + r);
-return (r);
 }
