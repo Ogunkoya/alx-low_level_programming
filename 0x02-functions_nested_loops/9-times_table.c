2a2d2a5b37516e7be68925e8f37cb20d15dcf1de
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-06 16:20:53 -0700

    Task 8

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,12 +17,16 @@ for (i = 0; i <= 9; i++)
 	for (j = 1; j <= 9; j++)
 	{
 		k = i * j;
-		if (i == 0)
+		if (j == 0)
 		{
 			_putchar(k + '0');
-			_putchar(32);
-			_putchar(32);
+		}
+		if (k < 10 && j != 0)
+		{
 			_putchar(44);
+			_putchar(32);
+			_putchar(32);
+			_putchar(k + '0');
 		}
 		else if (k >= 10)
 		{
@@ -30,13 +34,6 @@ for (i = 0; i <= 9; i++)
 			_putchar(32);
 			_putchar((k / 10) + '0');
 			_putchar((k % 10) + '0');
-		}
-		else
-		{
-			_putchar(44);
-			_putchar(32);
-			_putchar(32);
-			_putchar(k + '0');
 		}
 	}
 	_putchar('\n');
