f1ccd169489e8cf80f40924bb6617c6fa681470d
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-08 14:22:46 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - Check if paramter c represents an uppercase letter.
+* @a: Int to be multiple with a
+* @b: Int to be multiple with b
+* Return: Always 0.
+*/
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+return (result);
+}
