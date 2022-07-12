1f1f882f3655603e34b8c676f1e503a241e83257
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-12 15:07:45 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - print an array.
+* @a: array
+* @n: length of the array to be printed out
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	_putchar(10);
+}
