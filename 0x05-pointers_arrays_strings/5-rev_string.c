08d34d614bc74f47983a6a79cc881d2ac374b71d
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-12 14:18:28 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* rev_string - reverse a string.
+* @s: the string to be reversed.
+*/
+
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	int k;
+	char v1;
+	char v2;
+
+	k = 0;
+	while (s[k] != '\0')
+	{
+		k++
+	}
+	j = k - 1;
+	i = 0;
+	while (j > i)
+	{
+		v1 = s[i];
+		v2 = s[j];
+		s[i] = v2;
+		s[j] = v1;
+		j--;
+		i++;
+	}
+}
