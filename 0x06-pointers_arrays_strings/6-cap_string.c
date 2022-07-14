ed86c6a306e67e9fdf2e5ad07850b642fea4cd9b
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-14 14:39:47 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* string_toupper -  function that capitalizes all words of a string.
+*@n: cadena de caracteres.
+* Return: n.
+*/
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+		}
+	}
+	return (n);
+}
