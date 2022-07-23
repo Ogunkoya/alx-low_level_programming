bf83a54b66d0eeec26f6182c847718eb758fdc4d
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-23 14:32:59 -0700

    Quick fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,17 @@
 
 int main(int argc, char *argv[])
 {
-int num1, num2, prod;
+int i, prod = 1;
+
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-prod = num1 * num2;
-printf("%d\n", prod);
+for (i = 1; i < argc; i++)
+{
+prod *= atoi(argv[i]);
+}
+printf("%d\n", val);
 return (0);
 }
