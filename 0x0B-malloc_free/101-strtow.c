ff864f6d34825dee488fd74bedff0a11f64f9afa
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-27 03:20:41 -0700

    Tsk 3

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -31,6 +31,7 @@ char **strtow(char *str)
 {
 char **aout;
 unsigned int c, height, i, j, a1;
+
 if (str == NULL || *str == '\0')
 {
 return (NULL);
@@ -40,6 +41,7 @@ for (c = height = 0; str[c] != '\0'; c++)
 if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 {
 height++;
+}
 }
 aout = malloc((height + 1) * sizeof(char *));
 if (aout == NULL || height == 0)
@@ -73,5 +75,4 @@ aout[i][j] = '\0';
 }
 aout[i] = NULL;
 return (aout);
-}
 }
