dbcec8d571252b8a0da125bcf8e370ae635842fb
Wasiu Emmanuel Ogunkoya <godrace062@gmail.com>
2022-07-29 21:46:27 -0700

    Task 4

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
+int _putchar(char c);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
+void _puts(char *str);
 
 #endif
